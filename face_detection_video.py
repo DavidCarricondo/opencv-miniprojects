66aa9ae697c42498ea5aeb0046dd069c5e4ec6b9
DavidCarricondo <dacsmail00@gmail.com>
2020-10-02 09:54:20 +0200

    More scripts

---
+++ face_detection_video.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    _, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
